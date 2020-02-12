fe6834a51f8161792dc7a0692d2c929b86fa8b50
CeeDeeBee <cdbarnes98@gmail.com>
2020-02-12 18:02:19 -0500

    Added menu slide transition

--- Menu/Menu.js
+++ Menu/Menu.js
@@ -33,6 +33,7 @@ let menuItems = [
   Step 6: add the menu component to the DOM.
   
 */
+const clickDiv = document.querySelector('.click-div');
 
 function createMenuComponent(menuArray) {
   // Create elements
@@ -52,10 +53,17 @@ function createMenuComponent(menuArray) {
 
   // Add event listener
   const menuButton = document.querySelector('.menu-button');
-  menuButton.addEventListener('click', () => menu.classList.toggle('menu--open'));
+  menuButton.addEventListener('click', () => {
+    menu.classList.toggle('menu--open');
+    clickDiv.style.display = 'block';
+  });
 
   return menu
 }
 
 const body = document.querySelector('body');
 body.append(createMenuComponent(menuItems));
+
+// Collapse menu of it's open and user clicks outside
+const menu = document.querySelector('.menu');
+clickDiv.addEventListener('click', () => menu.classList.toggle('menu--open'));
